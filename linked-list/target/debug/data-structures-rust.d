8ca312bf2a40ceabf18addf6e04506eb85f8c4ad
Mx. Kas Perch <the@nodebotani.st>
2019-12-27 11:15:39 -0600

    removing target

--- linked-list/target/debug/data-structures-rust.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/Nodebotanist/code/rust/data-structures-rust/target/debug/data-structures-rust: /mnt/c/Users/Nodebotanist/code/rust/data-structures-rust/src/main.rs
--- linked-list/target/debug/deps/data_structures_rust-fe9de7aead5991f3.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/Nodebotanist/code/rust/data-structures-rust/target/debug/deps/data_structures_rust-fe9de7aead5991f3: src/main.rs
-
-/mnt/c/Users/Nodebotanist/code/rust/data-structures-rust/target/debug/deps/data_structures_rust-fe9de7aead5991f3.d: src/main.rs
-
-src/main.rs:
